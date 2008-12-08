4cc719f3cdd254c26f7fd9476bd5edde04aa117d
awflasher <awflasher@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-12-08 06:31:50 +0000

    First update the simple plugin :)
    git-svn-id: https://plugins.svn.wordpress.org/parent-category-toggler/trunk@78166 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ parent-category-toggler.php
@@ -0,0 +1,41 @@
+<?php
+/*
+Plugin Name: Parent Category Toggler
+Plugin URI: http://www.awflasher.com/blog/
+Description: Automatically toggle the parent categories when a sub category is selected.
+Version: 1.0
+Author: Aw Guo
+Author URI: http://www.awflasher.com/blog/
+Licence: GPL
+*/
+
+function super_category_toggler() {
+	echo '
+		<script>
+		jQuery(".selectit input").change(function(){
+			var $chk = jQuery(this);
+			var ischecked = $chk.is(":checked");
+			$chk.parent().parent().siblings().children("label").children("input").each(function(){
+var b = this.checked;
+ischecked = ischecked || b;
+})
+			checkParentNodes(ischecked, $chk);
+		});
+		function checkParentNodes(b, $obj)
+		{
+			$prt = findParentObj($obj);
+			if ($prt.length != 0)
+			{
+			 $prt[0].checked = b;
+			 checkParentNodes(b, $prt);
+			}
+		}
+		function findParentObj($obj)
+		{
+			return $obj.parent().parent().parent().prev().children("input");
+		}
+		</script>
+		';
+}
+add_action('admin_footer', 'super_category_toggler');
+?>
